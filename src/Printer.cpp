72b65cec6b1b160fa8a3c7b8f6187b2529dc5991
KyrillZjdanov <161046067+KyrillZjdanov@users.noreply.github.com>
2024-03-07 16:07:56 +0100

    getter en work die werk aan printer geeft

--- src/Printer.cpp
+++ src/Printer.cpp
@@ -1,1 +1,20 @@
 #include "Printer.h"
+#include <chrono>
+#include <thread>
+#include "Job.h"
+bool Printer::work(Job *job) {
+    ready = false;
+    double printingTime = job->getPagecount() / speed;
+    std::this_thread::sleep_for(std::chrono::minutes(static_cast<int>(printingTime)));
+    ready = true;
+
+    return true;
+}
+
+bool Printer::isReady() const {
+    return ready;
+}
+
+const std::string &Printer::getName() const {
+    return name;
+}
