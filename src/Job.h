a108d6ba2ef9ad18e3c43e7db04223c6bdd91e0d
KyrillZjdanov <161046067+KyrillZjdanov@users.noreply.github.com>
2024-03-07 16:06:47 +0100

    Update Job.h
    alleen getters bijgemaakt

--- src/Job.h
+++ src/Job.h
@@ -10,6 +10,18 @@ private:
     std::string username;
 public:
     Job(int jn, int pc, std::string un) : jobnumber(jn), pagecount(pc), username(un) {};
+
+    int getPagecount() const {
+        return pagecount;
+    }
+
+    int getJobnumber() const {
+        return jobnumber;
+    }
+
+    const std::string &getUsername() const {
+        return username;
+    }
 };
 
 
