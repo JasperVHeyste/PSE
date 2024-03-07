601ae061c3c7e5a7e670c42cc9dbf411cacc48ea
KyrillZjdanov <161046067+KyrillZjdanov@users.noreply.github.com>
2024-03-07 16:08:33 +0100

    zelfde als cpp

--- src/Printer.h
+++ src/Printer.h
@@ -1,14 +1,25 @@
 #ifndef UNTITLED_PRINTER_H
 #define UNTITLED_PRINTER_H
 #include <string>
-
+#include <iostream>
+class Job;
 class Printer {
 private:
     std::string name;
     int emission;
     int speed; // pages per minute
+    std::string username;
+    int jobnumber;
+    int pagecount;
+    bool ready;
 public:
-    Printer(std::string n, int e, int s) : name(n), emission(e), speed(s) {};
+    Printer(std::string n, int e, int s) : name(n), emission(e), speed(s), ready(true) {};
+
+    bool work(Job *job);
+
+    bool isReady() const;
+
+    const std::string &getName() const;
 };
 
 
