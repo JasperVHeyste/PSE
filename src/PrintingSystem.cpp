1bb148e98c6a7871133adbcf267a851f3f75d3eb
JasperVHeyste <149973012+JasperVHeyste@users.noreply.github.com>
2024-03-07 17:57:21 +0100

    Update PrintingSystem.cpp

--- src/PrintingSystem.cpp
+++ src/PrintingSystem.cpp
@@ -16,8 +16,8 @@ void PrintingSystem::createJob(int jobnumber, int pagecount, string username){
     jobs.enqueue(newJob);
 }
 
-void PrintingSystem::implementXML(const char* file) {
-    vector<map<string,string>> input = readXML(file);
+void PrintingSystem::implementXML(const char* file, std::ostream& outputstream) {
+    vector<map<string,string>> input = readXML(file, outputstream);
 
     for (unsigned int i = 0; i < input.size(); i++){
         map<string, string> object = input[i];
@@ -32,9 +32,9 @@ void PrintingSystem::implementXML(const char* file) {
         }
 
         if (object["type"] == "job"){
-            int jobnumber = stoi(object["jobnumber"]);
-            int pagecount = stoi(object["pagecount"]);
-            string username = object["username"];
+            int jobnumber = stoi(object["jobNumber"]);
+            int pagecount = stoi(object["pageCount"]);
+            string username = object["userName"];
 
             createJob(jobnumber, pagecount, username);
             cout << "created job with jobnumber " << jobnumber << endl;
@@ -49,7 +49,7 @@ void PrintingSystem::manualJob() {
                 Job *job = jobs.dequeue();
                 p->work(job);
                 cout << "Printer " << "'" << p->getName() << "'" << " finished job:" << "\n    Number: " << job->getJobnumber() << "\n    Submitted by '" <<
-                job->getUsername() << "'" << endl << "    " <<job->getPagecount() << " pages" << endl;
+                     job->getUsername() << "'" << endl << "    " <<job->getPagecount() << " pages" << endl;
             }
         }
     }
